48ad9e00cdc298c423d08369df4a31893a67aa9d
Ricardo Mauro <ricardo.demauro@outlook.com>
2018-02-02 15:49:09 +0000

    webdriver sample

--- src/BookTracker.RunnerConsole/Program.cs
+++ src/BookTracker.RunnerConsole/Program.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BookTracker.RunnerConsole
@@ -10,6 +15,38 @@ namespace BookTracker.RunnerConsole
     {
         static void Main(string[] args)
         {
+            IWebDriver webDriver = new ChromeDriver();
+
+            SafeContext(driver =>
+            {
+                driver.Navigate().GoToUrl("https://www.google.com.br");
+
+                var element = driver.FindElement(By.Name("q"));
+
+                element.SendKeys("execute automation");
+                element.SendKeys(Keys.Enter);
+
+                Thread.Sleep(6000);
+            }, webDriver);
+
+            
+        }
+
+        static void SafeContext(Action<IWebDriver> action, IWebDriver webDriver)
+        {
+            try
+            {
+                action(webDriver);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.Message);
+                throw;
+            }
+            finally
+            {
+                webDriver.Quit();
+            }
         }
     }
 }
