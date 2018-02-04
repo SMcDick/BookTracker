3fd6307564e835573d2a0d6267a5d41f0a9ee1bc
Ricardo Mauro <ricardo.demauro@gmail.com>
2018-02-04 16:25:52 -0200

    adding websocket project.

---
+++ src/BookTracker.Websocket/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using WebSocketSharp;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookTracker.Websocket
+{
+    public partial class Form1 : Form
+    {
+        private WebSocket ws;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ws = new WebSocketSharp.WebSocket(textBoxUri.Text);
+            
+            ws.OnMessage += Ws_OnMessage;
+            ws.OnError += Ws_OnError;
+            ws.Connect();
+           // ws.Send("BALUS");
+        }
+
+        private void Ws_OnError(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        private void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine(e.Data);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(ws.IsAlive)
+            {
+                ws.Close();
+                ws = null;
+            }
+        }
+
+        private void buttonSendMessage_Click(object sender, EventArgs e)
+        {
+            ws.Send(textBoxMessage.Text);
+        }
+
+        private void SetLogText(string text)
+        {
+            textBoxLog.Text = $"{textBoxLog.Text}{Environment.NewLine}{text}";
+        }
+    }
+}
